f08f7672a2628acdbcd6ec01f517f53b893775a7
Prithvi Chouhan <prithvichouhand9ma@gmail.com>
2022-03-20 21:44:52 +0530

    Add: method to get top ten block and save in state

--- src/components/block-explorer/block.js
+++ src/components/block-explorer/block.js
@@ -1,4 +1,5 @@
 import React from "react";
+import web3 from "../../shared/web3/web3";
 const getBlockDetailsByNumber = (i, opt = true) => {
   return new Promise((resolve, reject) => {
     web3.eth.getBlock(i, opt, (err, data) => {
@@ -25,11 +26,27 @@ class Blocks extends React.Component {
     return blockNumber;
   };
   
+  getTopTenBlocks = async (curr_block_no) => {
+    const block = this.state.blocks.slice();
+    let max_blocks = 10;
+
+    if (curr_block_no < max_blocks) max_blocks = curr_block_no;
+
+    for (var i = 0; i < max_blocks; i++, curr_block_no--) {
+      var currBlockNo = await getBlockDetailsByNumber(curr_block_no, true);
+      block.push(currBlockNo);
+    }
+
+    this.setState({
+      loading: false,
+      blocks: block,
+    });
+  };
+
     render() {
     return <div>Block</div>;
     }
 }
   
-  
-  export default (Blocks);
+export default Blocks;
   
