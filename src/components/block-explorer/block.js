69db93964bb58f2efea713edc398f97558fe1be5
Prithvi Chouhan <prithvichouhand9ma@gmail.com>
2022-03-20 21:52:04 +0530

    Add: set interval in component did mount

--- src/components/block-explorer/block.js
+++ src/components/block-explorer/block.js
@@ -1,5 +1,16 @@
 import React from "react";
 import web3 from "../../shared/web3/web3";
+import {
+  Paper,
+  Box,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
+
 const getBlockDetailsByNumber = (i, opt = true) => {
   return new Promise((resolve, reject) => {
     web3.eth.getBlock(i, opt, (err, data) => {
@@ -17,7 +28,9 @@ class Blocks extends React.Component {
       this.state = {
         blocks: [],
         curr_block: null,
- 
+      displayTransaction: false,
+      loading: true,
+      blockNo: null,
       };
     }
   
@@ -32,8 +45,8 @@ class Blocks extends React.Component {
 
     if (curr_block_no < max_blocks) max_blocks = curr_block_no;
 
-    for (var i = 0; i < max_blocks; i++, curr_block_no--) {
-      var currBlockNo = await getBlockDetailsByNumber(curr_block_no, true);
+    for (let i = 0; i < max_blocks; i++, curr_block_no--) {
+      let currBlockNo = await getBlockDetailsByNumber(curr_block_no, true);
       block.push(currBlockNo);
     }
 
@@ -51,6 +64,25 @@ class Blocks extends React.Component {
     });
 
     await this.getBlocks(curr_block_no);
+  }
+
+  async componentDidMount() {
+    setInterval(async () => {
+      let curr_block_no = await this.web3_eth_getBlockNumber();
+
+      if (curr_block_no > this.state?.blocks[0]?.number) {
+        const block = await getBlockDetailsByNumber(curr_block_no, true);
+
+        if (block) {
+          this.state.blocks.pop();
+          this.state.blocks.unshift(block);
+        }
+
+        this.setState({
+          curr_block: curr_block_no,
+        });
+      }
+    }, 3000);
   }
     render() {
     return <div>Block</div>;
