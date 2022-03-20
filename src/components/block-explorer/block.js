71dfff5850ceba3991b4aaff996db2a0bfb3bae2
Prithvi Chouhan <prithvichouhand9ma@gmail.com>
2022-03-20 21:21:37 +0530

    Create: component block to render top 10 blocks

---
+++ src/components/block-explorer/block.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Blocks extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        blocks: [],
+        curr_block: null,
+ 
+      };
+    }
+  
+  
+    render() {
+      return (
+        <div>
+          Block
+        </div>
+      );
+    }
+}
+  
+  
+  export default (Blocks);
+  
